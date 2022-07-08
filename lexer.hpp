2ef4cef78da03b75cc09c2d47ac57febc427f9f0
Bdbmzwsc <88311279+Bdbmzwsc@users.noreply.github.com>
2022-07-08 14:32:30 +0800

    write some function for lexer

--- lexer.hpp
+++ lexer.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
 using namespace std;
 class lexer
 {
@@ -35,6 +36,36 @@ public:
 
 			 }
 		 }
+		 bool next_code(string str) {
+			 int len = str.length() - 1;
+			 if (str.length() < sourceCode.length() && str.compare(sourceCode.substr(0, str.length())) == 0) {
+				 sourceCode.erase(0, 2);
+				 return true;
+			 }
+			 else
+			 {
+				 if (str.length() == sourceCode.length()) return true;
+			 }
+			 return false;
+		 }
+		 string scan_before_token(string token)
+		 {
+			 vector<string> s = split(sourceCode, token);
+			 if (s.size() < 2)
+			 {
+				 cout<<"unreachable!";
+				 throw "Exception";
+			 }
+			 skipsrc(s[0].length());
+			 return s[0];
+		 }
+		 void skipsrc(int n) {
+			 sourceCode = sourceCode.substr(n);
+		 }
+		 int GetLineNum()
+		 {
+			 return lineNum;
+		 }
 		 bool isIgnored() {
 			 bool isIgnored = false;
 			 auto isWhiteSpace = [](char c) -> bool {
@@ -62,6 +93,26 @@ public:
 
 
 private:
+	static vector<string> split(string& str, string& pattern)
+	{
+		vector<string> res;
+		if (str == "")
+			return res;
+		//在字符串末尾也加入分隔符，方便截取最后一段
+		string strs = str + pattern;
+		size_t pos = strs.find(pattern);
+
+		while (pos != strs.npos)
+		{
+			string temp = strs.substr(0, pos);
+			res.push_back(temp);
+			//去掉已分割的字符串,在剩下的字符串中进行分割
+			strs = strs.substr(pos + 1, strs.size());
+			pos = strs.find(pattern);
+		}
+
+		return res;
+	}
 	map<int, string> tokenNameMap = {
 		{TOKEN_NUM,"%"},
 		{TOKEN_EOF,"EOF"},
